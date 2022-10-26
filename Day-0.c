2a8c372feb5c475a2e4b03f55832e030987cef93
Nightburnn <nightburn2003@gmail.com>
2022-10-26 00:47:55 -0700

    Day 0

---
+++ Day-0.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    // Declare a variable named 'input_string' to hold our input.
+    char input_string[105]; 
+  
+
+    // Read a full line of input from stdin and save it to our variable, input_string.
+    scanf("%[^\n]", input_string); 
+   
+
+    // Print a string literal saying "Hello, World." to stdout using printf.
+
+    printf("Hello, World.\n");
+   
+
+    // TODO: Write a line of code here that prints the contents of input_string to stdout.
+
+    printf("HackerRank is the best!", input_string);
+    //This happen to be the secret code, the first answer should be Welcome to 30 Days of Code!
+    //
+
+    
+
+    
+
+    return 0;
+
+}
+
